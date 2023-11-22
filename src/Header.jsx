778cd44297c9f94d52f476fa6368e41760f2adf1
Ahmet Karahasanoglu <ahmetkarahasan9@gmail.com>
2023-11-22 14:30:09 +0300

    fix: image directories 7

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header() {
     return (
         <header className="header">
-            <img src="https://ahmetkarahasanoglu.github.io/my-travel-journal-with-reactjs/src/assets/earth-icon-for-header.svg" alt="Earth Icon" className="earth-icon"/> <p>my travel journal.</p>
+            <img src="/assets/earth-icon-for-header.svg" alt="Earth Icon" className="earth-icon"/> <p>my travel journal.</p>
         </header>
     )
 }
